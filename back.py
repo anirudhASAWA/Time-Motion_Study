f69afc8c82a849f86392b0506813762994c01fad
anirudhASAWA <96370504+anirudhASAWA@users.noreply.github.com>
2025-03-03 14:44:22 +0530

    Update back.py

--- back.py
+++ back.py
@@ -170,6 +170,5 @@ def delete_project(filename):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
